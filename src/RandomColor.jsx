a06adf55ea39ff48a73d63b54489ecc55b8b71f7
Kennedy Senyo Dordoe <kennedysenyo@gmail.com>
2025-01-06 20:47:19 +0000

    style the buttons

--- src/RandomColor.jsx
+++ src/RandomColor.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const RandomColor = () => {
   const [colorFormat, setColorFormat] = useState("hex")
-  const [color, setColor] = useState("")
+  const [color, setColor] = useState("#ffffff")
+
 
   function generateHEXColor() {
     generateRandomColor("hex")
@@ -13,8 +14,7 @@ const RandomColor = () => {
     setColorFormat("rgb");
     generateRandomColor("rgb")
   }
- 
-  
+    
   function generateRandomColor(format) {
     const currentFormat = format || colorFormat;
 
@@ -33,13 +33,14 @@ const RandomColor = () => {
   
   document.getElementById("root").style.backgroundColor = color;
 
-  console.log(color);
 
   return (
     <>
       <div className="container">
+        <div>
         <button onClick={generateHEXColor}>Create HEX Color</button>
         <button onClick={generateRGBColor}>Create RGB Color</button>
+        </div>
         <button onClick={() => generateRandomColor()}>Generate Random Color</button>
       </div>
       <h1>{color}</h1>
