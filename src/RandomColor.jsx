b1c40c9e691c739d35085958fbdcacce9d9a9218
Kennedy Senyo Dordoe <kennedysenyo@gmail.com>
2025-01-06 20:04:47 +0000

    make functional random color generator

--- src/RandomColor.jsx
+++ src/RandomColor.jsx
@@ -1,8 +1,48 @@
+import { useState } from "react";
 
 const RandomColor = () => {
+  const [colorFormat, setColorFormat] = useState("hex")
+  const [color, setColor] = useState("")
+
+  function generateHEXColor() {
+    generateRandomColor("hex")
+    setColorFormat("hex");
+  }
+  
+  function generateRGBColor() {
+    setColorFormat("rgb");
+    generateRandomColor("rgb")
+  }
+ 
+  
+  function generateRandomColor(format) {
+    const currentFormat = format || colorFormat;
+
+    if (currentFormat === "hex") {     
+      const hexColor = Array.from({ length: 6 }, () =>
+        Math.floor(Math.random() * 16).toString(16).toUpperCase()
+      ).join("");
+      setColor(`#${hexColor}`);
+    } else {      
+      const red = Math.floor(Math.random() * 256);
+      const green = Math.floor(Math.random() * 256);
+      const blue = Math.floor(Math.random() * 256);
+      setColor(`rgb(${red}, ${green}, ${blue})`);
+    }   
+  }
+  
+  document.getElementById("root").style.backgroundColor = color;
+
+  console.log(color);
+
   return (
     <>
-    <h1>This a random color component</h1>
+      <div className="container">
+        <button onClick={generateHEXColor}>Create HEX Color</button>
+        <button onClick={generateRGBColor}>Create RGB Color</button>
+        <button onClick={() => generateRandomColor()}>Generate Random Color</button>
+      </div>
+      <h1>{color}</h1>
     </>
   )
 }
